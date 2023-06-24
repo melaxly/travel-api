1e97742b4ffdeb89ca49499d3ff0d3a28329604a
melaxly <mcodestars@gmail.com>
2023-06-24 01:07:55 +0100

    all database schema done

---
+++ app/Models/Tour.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class Tour extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'travel_id',
+        'name',
+        'starting_date',
+        'ending_date',
+        'price',
+    ];
+
+    public function price()
+    {
+        return Attribute::make(
+            get: fn ($value) => $value / 100,
+            set: fn ($value) => $value * 100
+        );
+    }
+}
